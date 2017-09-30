5421caed88e6e9c8b641105c4487053bda91e678
Beto Rincon <aluisrincon@unisangil.edu.co>
2017-09-30 18:15:35 -0500

    a

---
+++ animaciones.js
@@ -0,0 +1,86 @@
+//atributos
+$adelante = $('#adelante');
+$atras = $('#atras');
+$refrescar = $('#refrescar');
+
+//eventos
+
+$adelante.click(function () {
+     
+  // alert(window.location.href);
+    var ubicacion = "http://localhost/ova/";
+ 
+    switch (window.location.href) {
+        case ubicacion + "index.html":
+            window.location.replace(ubicacion + "densidad1.html");
+            break;    
+            
+    }
+});
+
+$atras.click(function () {
+    
+ // alert(window.location.href);
+   var ubicacion = "http://localhost/ova/";
+
+   switch (window.location.href) {
+        case ubicacion + "index.html":
+           window.location.replace(window.location.href);
+           break;    
+        case ubicacion + "densidad1.html":
+           window.location.replace(ubicacion+"index.html");
+           break;    
+           
+   }
+});
+
+$refrescar.click(function () {
+    reload();
+    function loadPage() {
+        location.reload();
+    }
+    /* https://www.w3schools.com/js/js_timing.asp */
+    setTimeout(loadPage(), 3000);
+    
+});
+
+$adelante.mouseenter(function () {
+    animacion1("adelante");
+});
+$adelante.mouseleave(function () {
+    animacion2("adelante");
+});
+$atras.mouseenter(function () {
+    animacion1("atras");
+});
+$atras.mouseleave(function () {
+    animacion2("atras");
+});
+$refrescar.mouseenter(function () {
+    reload();
+})
+
+
+//animaciones
+
+function reload() {
+    TweenMax.fromTo($refrescar, 1, {transformOrigin: 'center',rotation: 0,ease: Power2.easeOut},{transformOrigin: 'center',rotation: 360})  
+}
+
+function animacion1(a) {
+    if (a == "adelante") {
+        TweenMax.to($adelante, 0.5, {scale:1.2})  
+    }
+    if (a == "atras") {
+        TweenMax.to($atras, 0.5, {scale:1.2})  
+    }
+    
+}
+function animacion2(a) {
+    if(a=="adelante")
+        TweenMax.to($adelante, 0.5, { scale: 1.0 })
+    if (a == "atras") {
+        TweenMax.to($atras, 0.5, { scale: 1.0 })
+    }
+}
+
---
+++ main.js
@@ -0,0 +1,22 @@
+//import Vue from 'vue';
+
+/*var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        
+    }
+});
+*/
+
+
+function siguiente() {
+    alert("click");
+}
+
+
+
+
+
+
+            
